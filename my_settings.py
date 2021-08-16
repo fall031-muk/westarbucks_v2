1af6f13dd90155e13747f2ffb38f359ab974aa1d
fall031 <fall900802@gmail.com>
2021-08-16 16:55:59 +0900

    Add: Django Project Setting_v2

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks_v2',
+        'USER': 'root',
+        'PASSWORD': 'dbsgusanr12!A',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-onxt$^8nsl-_shn^mcyy1h^e3^d^mcqi6m&amko4q9%hhy(wkl'
